8a6dbccdf4bc0bfba5e36fecfb075181f046dea2
XiaoXKKK <37475561+XiaoXKKK@users.noreply.github.com>
2023-04-03 16:34:37 +0800

    Load & Save Data

---
+++ js/data.js
@@ -0,0 +1,41 @@
+function saveData() {
+    const formElements = document.querySelectorAll("input, textarea, select");
+    const formData = {};
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+        console.log(element);
+        const name = element.name;
+        const value = element.value;
+        console.log(name);
+        console.log(value);
+        if (name) {
+            formData[name] = value;
+        }
+    }
+    const formDataString = JSON.stringify(formData);
+    const blob = new Blob([formDataString], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "form-data.json";
+    link.click();
+  }
+  
+  function loadData(file) {
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      const formDataString = event.target.result;
+      const formData = JSON.parse(formDataString);
+      const formElements = document.querySelectorAll("input, textarea, select");
+      for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+        const name = element.name;
+        const value = formData[name];
+        if (value !== undefined) {
+          element.defaultValue = value;
+          element.value = value;
+        }
+      }
+    };
+    reader.readAsText(file);
+  }
